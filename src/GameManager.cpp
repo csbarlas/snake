4583edb5187631427aa4237e896b650a53594f1c
Chris Barlas <62313241+csbarlas@users.noreply.github.com>
2025-06-06 22:52:11 -0400

    Crude frame time calculation and CPU yielding

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -2,6 +2,11 @@
 
 #include <SDL3/SDL.h>
 
+namespace {
+    constexpr int FRAME_RATE{60};
+    constexpr Uint64 FRAME_TIME{(int)1e9 / FRAME_RATE};
+}
+
 GameManager::GameManager() {
     SDL_Log("GameManager initialized");
 }
@@ -40,6 +45,8 @@ void GameManager::enterMainLoop() {
     bool quit{false};
 
     while (!quit) {
+        Uint64 startTime{SDL_GetTicksNS()};
+
         bool shouldQuit{processEvents()};
         if (shouldQuit) {
             break;
@@ -47,6 +54,24 @@ void GameManager::enterMainLoop() {
 
         update();
         render();
+
+        Uint64 frameTime{0};
+        frameTime = SDL_GetTicksNS() - startTime;
+
+        if (frameTime < FRAME_TIME) {
+            Uint64 sleepTimeNS{FRAME_TIME - frameTime};
+
+            Uint64 targetTimeNS{SDL_GetTicksNS() + sleepTimeNS};
+            // spin wait, not great for CPU
+            while (SDL_GetTicksNS() < targetTimeNS) { }
+
+            // Can yield CPU but incurs overhead
+            //SDL_DelayNS(sleepTimeNS);
+        }
+
+        Uint64 totalFrameTimeNS{SDL_GetTicksNS() - startTime};
+        double framesPerSecond{(double)1e9 / totalFrameTimeNS};
+        SDL_Log("FPS: %.2f", framesPerSecond);
     }
 }
 
